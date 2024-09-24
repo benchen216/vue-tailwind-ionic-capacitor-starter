8f1d62354611d302bcc0e87c83c9de2839192b5e
benjamin chen <benchen216@gmail.com>
2024-09-24 20:02:31 +0800

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'vue-tailwind-ionic-capacitor-starter',
+  webDir: 'dist'
+};
+
+export default config;
